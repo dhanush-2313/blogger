b7c5276710d6a01689aa25903294c512f2420c0b
dhanush-2313 <dhanusu006@gmail.com>
2024-09-02 15:01:33 +0530

    2routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,76 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { sign } from "hono/jwt";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/apiv1/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-export default app
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+        name: body.name,
+      },
+    });
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    return c.status(403);
+  }
+});
+
+app.post("/apiv1/user/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const body = await c.req.json();
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    if (!user) {
+      c.status(403);
+      return c.json({ error: "user not found" });
+    }
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    return c.status(403);
+  }
+});
+
+app.post("/apiv1/blog", (c) => {
+  return c.text("blog post route");
+});
+
+app.put("/apiv1/blog", (c) => {
+  return c.text("blog put route");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("single blog route");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("all blogs route");
+});
+
+export default app;
