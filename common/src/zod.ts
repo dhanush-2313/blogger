1f690c9e14b2cbe1c430559ead08aa572e266ffb
dhanush-2313 <dhanusu006@gmail.com>
2024-09-02 19:29:19 +0530

    common

--- common/src/zod.ts
+++ common/src/zod.ts
@@ -13,19 +13,19 @@ export const signInBody = z.object({
   password: z.string().min(6),
 });
 
-export type SigninInput = z.infer<typeof signInBody>;
-
 export const createBlogInput = z.object({
   title: z.string(),
   content: z.string(),
 });
-
-export type CreateBlogInput = z.infer<typeof createBlogInput>;
 
 export const updateBlogInput = z.object({
   title: z.string(),
   content: z.string(),
   id: z.string(),
 });
+
+export type SigninInput = z.infer<typeof signInBody>;
+
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
 
 export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
